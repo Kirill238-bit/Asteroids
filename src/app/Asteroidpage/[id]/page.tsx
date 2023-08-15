42644cfe42ac357022309c029b21b141371293b3
kirill <kirill@yandery>
2023-08-15 10:59:33 +0300

    Feat(app,styles)made full Asteroidpage

--- src/app/Asteroidpage/[id]/page.tsx
+++ src/app/Asteroidpage/[id]/page.tsx
@@ -1,13 +1,59 @@
-import style from '@/styles/Asteroidpage.module.scss'
+import style from '@/styles/asteroidpage.module.scss'
+import Image from '../../../../node_modules/next/image';
+import Link from '../../../../node_modules/next/link';
 
-export default  async function AsteroidPage(id:any) {
+export default  async function AsteroidPage({params:{id}}:any) {
+
     async function getAsteroid(id:any){
-        const respons=await fetch(`https://api.nasa.gov/neo/rest/v1/neo/3542519?api_key=BDfzLDpe4sPx7D4cbDuYXSIhiMTC2SUFkcXwn6i1&asteroid_id=${+id}`)
+        const respons=await fetch(`https://api.nasa.gov/neo/rest/v1/neo/${id}?api_key=BDfzLDpe4sPx7D4cbDuYXSIhiMTC2SUFkcXwn6i1`)
 
         return respons.json();
     }
-    const Asteroid= await getAsteroid(id)
+    const asteroid=await getAsteroid(+id)
+    console.log(asteroid)
   return (
-    <div>page</div>
+    <div className={style.wrapper}>
+        <div className={style.left_conteiner}>
+            <div className={style.image}>
+                <Image
+                src='/images/asteroid.png'
+                alt='asteroid'
+                width={400}
+                height={460}
+                />
+            </div>
+            <div className={style.left_conteiner_title}>{asteroid.name}</div>
+            <div className={`${style.little} ${parseInt(asteroid.estimated_diameter.meters.estimated_diameter_min)<100 ? style.little_active : ''}`}>
+            <Image
+                src='/images/pngegg 1.png'
+                alt='asteroid'
+                width={22}
+                height={24}
+            />
+            </div>
+            <div className={`${style.big} ${parseInt(asteroid.estimated_diameter.meters.estimated_diameter_min)>=100 ? style.big_active : ''}`}>
+            <Image
+                src='/images/pngegg 2.png'
+                alt='asteroid'
+                width={36}
+                height={40}
+            />
+            </div>
+            <div className={style.left_conteiner_text}>
+                <div>{asteroid.estimated_diameter.meters.estimated_diameter_min} м</div>
+                <div className={`${style.dangerous} ${asteroid.is_potentially_hazardous_asteroid ? style.dangerous_active : ''}`}>⚠ Опасен</div>
+            </div>
+        </div>
+        <div className={style.right_conteiner}>
+            {asteroid.close_approach_data.map((item:any)=>(
+                <div key={item.close_approach_date} className={style.item}>
+                    <div className={style.item_title}>{item.orbiting_body}</div>
+                    <div className={style.item_text}>Время сближения с Землей:<div>{item.close_approach_date_full}</div></div>
+                    <div className={style.item_text}>Скорость относительно Земли:<div>{item.relative_velocity.kilometers_per_hour} км</div></div>
+                    <div className={style.item_text}>Расстояние:<div>{item.miss_distance.kilometers} км</div></div>
+                </div>
+                ))}
+        </div>
+    </div>
   )
 }
